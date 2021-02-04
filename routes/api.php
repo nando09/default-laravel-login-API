e305c0955a90d105dff1dadd4e0bcfbc4cad6c50
Fernando Batista <foliveira@expertvoice.com.br>
2021-02-04 15:49:26 -0300

    Start login backend API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+	Route::get('usuarios', [UserController::class,'all']);
+});
+
+Route::post('login', [UserController::class,'index']);
+
