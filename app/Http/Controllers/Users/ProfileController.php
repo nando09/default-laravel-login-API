6ec870d66d5db444c040bd3427105785b0f24fbb
Fernando <nando.online@live.com>
2021-03-18 13:31:44 -0300

    Fix CRUD profile

--- app/Http/Controllers/Users/ProfileController.php
+++ app/Http/Controllers/Users/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Users;
 use App\Models\Users\Profile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -15,21 +16,58 @@ class ProfileController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $validator = Validator::make($data, [
+                'name'          =>  ['required', 'max:255', 'unique:profiles']
+            ],
+            [
+                'name.required'         =>  'Nome obrigatório!',
+                'name.max'              =>  'Máximo de caracteres 255 para nome!',
+                'name.unique'           =>  'Já existe esse nome!'
+            ]
+        );
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return $errors;
+        }
+
+        $profile = Profile::create($data);
+        return $profile;
     }
 
     public function show($id)
     {
-        //
+        return Profile::findOrFail($id);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+        $validator = Validator::make($data, [
+                'name'          =>  ['required', 'max:255', 'unique:profiles,name,'. $id . ',id']
+            ],
+            [
+                'name.required'         =>  'Nome obrigatório!',
+                'name.max'              =>  'Máximo de caracteres 255 para nome!',
+                'name.unique'           =>  'Já existe esse nome!'
+            ]
+        );
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return $errors;
+        }
+
+        $profile = Profile::findOrFail($id);
+        $profile->update($data);
+        return $profile;
     }
 
     public function destroy($id)
     {
-        //
+         $profile = Profile::findOrFail($id);
+         $profile->delete();
+         return $profile;
     }
 }
