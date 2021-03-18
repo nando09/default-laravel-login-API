d22d166610ca0bb70820e5361eeb50674323e47e
Fernando <nando.online@live.com>
2021-03-18 12:35:50 -0300

    Fix join user and profile

--- database/migrations/2021_03_18_153317_add_id_profile_table_users.php
+++ database/migrations/2021_03_18_153317_add_id_profile_table_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfilesTable extends Migration
+class AddIdProfileTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateProfilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('profile_id')->references('id')->on('profiles');
         });
     }
 
@@ -27,6 +25,8 @@ class CreateProfilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profiles');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['profile_id']);
+        });
     }
 }
